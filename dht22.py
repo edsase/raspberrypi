83dcdfda495bae2ca0796fbb928c5a576837a8a6
Edmond Sesay <edmondssesay@yahoo.com>
2019-02-10 15:58:35 +0100

    minor bug fixes

--- dht22.py
+++ dht22.py
@@ -7,14 +7,14 @@ DURATION = 20
 end_time = time.time() + DURATION
 
 
-try:
-    while time.time() < end_time:                # Loop will run forever
-        humi, temp = dht.read_retry(dht.DHT22, 8)  # Reading humidity and temperature
-        print('Temp: {0:0.1f}*C  Humidity: {1:0.1f}%'.format(temp, humi)) 
-        sleep(3)
-# If keyboard Interrupt is pressed
-except KeyboardInterrupt:
-    pass  			# Go to next line
+# try:
+#     while time.time() < end_time:                # Loop will run forever
+#         humi, temp = dht.read_retry(dht.DHT22, 8)  # Reading humidity and temperature
+#         print('Temp: {0:0.1f}*C  Humidity: {1:0.1f}%'.format(temp, humi)) 
+#         sleep(3)
+# # If keyboard Interrupt is pressed
+# except KeyboardInterrupt:
+#     pass  			# Go to next line
 
 
 def get_dht22_data(interval):
