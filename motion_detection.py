58e432c27cb2f9f8fe065b5c030fc31835362b93
Edmond Sesay <edmondssesay@yahoo.com>
2019-02-10 13:27:21 +0100

    motion_detection.py added

---
+++ motion_detection.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+# GPIO.setup(23, GPIO.IN)
+channel = GPIO.wait_for_edge(23, GPIO.RISING)
+
+while True:
+    if channel:
+        print('Motion detected')
+
