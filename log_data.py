89ded0d55b6668915e77162b027d4faa90da6df3
Edmond Sesay <edmondssesay@yahoo.com>
2019-02-10 20:52:05 +0100

    minor bug fixes to log_data.py

--- log_data.py
+++ log_data.py
@@ -8,21 +8,29 @@ LOG_FILE = "../sensor-data/dht22.log"
 
 #  log all data to a file
 
-def log_data():
+def log_data(interval):
     #  check if dht22.log file exists, otherwise create it using w+
-    with open(LOG_FILE, 'w+') as logfile:
+    logfile = open(LOG_FILE, 'w+') 
+
+    while True:
         # read data
         data = get_dht22_data()
         # convert data to csv string
         csv_string = '{timestamp},{humidity},{temperature}\n'.format(**data._asdict())
-                
-        # save data in file, add headers if file is empty
-        if os.path.getsize(LOG_FILE) > 0:    
-            logfile.write(csv_string)
-        else:
-            # write headers
-            file_headers = "Timestamp,Humidity,Temperature\n"
-            logfile.write(file_headers + csv_string)
+        try:    
+            # save data in file, add headers if file is empty
+            if os.path.getsize(LOG_FILE) > 0:    
+                logfile.write(csv_string)
+            else:
+                # write headers
+                file_headers = "Timestamp,Humidity,Temperature\n"
+                logfile.write(file_headers + csv_string)
+            sleep(interval)
+        except:
+            pass
+        finally:
+            logfile.close()
+
 
 # every x intervals, copy logged data in file to usb folder if usb is in drive
 
@@ -30,5 +38,4 @@ def log_data():
 
 
 if __name__ == "__main__":
-    while 1:
-        log_data()
+    log_data(3)
