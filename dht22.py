964eb0110d25f4c97fb1635808f5a41ce12effcf
Edmond Sesay <edmondssesay@yahoo.com>
2019-02-10 12:48:35 +0100

    led_brightness.py minor bug fixes

--- dht22.py
+++ dht22.py
@@ -9,7 +9,7 @@ end_time = time.time() + DURATION
 try:
     while time.time() < end_time:                # Loop will run forever
         humi, temp = dht.read_retry(dht.DHT22, 8)  # Reading humidity and temperature
-        print 'Temp: {0:0.1f}*C  Humidity: {1:0.1f}%'.format(temp, humi) 
+        print('Temp: {0:0.1f}*C  Humidity: {1:0.1f}%'.format(temp, humi)) 
         sleep(3)
 # If keyboard Interrupt is pressed
 except KeyboardInterrupt:
