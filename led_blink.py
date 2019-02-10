c347b086ab82e4ce38f0ce4e9cc35b6d8be99928
Edmond Sesay <edmondssesay@yahoo.com>
2019-02-10 13:47:53 +0100

    motion_detection.py minor bug fixes

--- led_blink.py
+++ led_blink.py
@@ -1,25 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(19, GPIO.OUT)
-
-DURATION = 20
-endtime = time.time() + DURATION
-
-try:
-    while time.time() < endtime:
-        GPIO.output(19, True)
-        time.sleep(0.5)
-        GPIO.output(19, False)
-        time.sleep(0.5)
-    
-finally:\
-    # cleanup
-    GPIO.cleanup()
-
-
 def blink(duration=6):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(19, GPIO.OUT)
     endtime = time.time() + duration
 
     try:
