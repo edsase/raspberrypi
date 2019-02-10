92f53481ebc6095d0f4f9030903686855992f55f
Edmond Sesay <edmondssesay@yahoo.com>
2019-02-10 13:32:20 +0100

    motion_detection.py minor bug fixes

--- motion_detection.py
+++ motion_detection.py
@@ -1,4 +1,6 @@
 import RPi.GPIO as GPIO
+from time import sleep
+
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(23, GPIO.IN)
@@ -7,4 +9,5 @@ channel = GPIO.wait_for_edge(23, GPIO.RISING)
 while True:
     if channel:
         print('Motion detected')
+        sleep(1)
 
