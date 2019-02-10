c5462680f38f14d04e8259f221bdbdd56d903652
Edmond Sesay <edmondssesay@yahoo.com>
2019-02-10 15:51:35 +0100

    dht22.py modifications

--- dht22.py
+++ dht22.py
@@ -1,6 +1,7 @@
 import Adafruit_DHT as dht    # Importing Adafruit library for DHT22
 from time import sleep           # Impoting sleep from time library to add delay
-import time
+import time, datetime
+from collections import namedtuple
 
 DURATION = 20
 end_time = time.time() + DURATION
@@ -14,3 +15,24 @@ try:
 # If keyboard Interrupt is pressed
 except KeyboardInterrupt:
     pass  			# Go to next line
+
+
+def get_dht22_data(interval):
+    """ gets data from the dht22 sensor every interval seconds"""
+
+    data_point = namedtuple('datapoint', 'date', 'humidity', 'temperature' )
+
+    try:
+        while True:                # Loop will run forever
+            humi, temp = dht.read_retry(dht.DHT22, 8)  # Reading humidity and temperature
+            # get current time
+            dt = datetime.datetime.now().strftime('%Y-%M-%d %H:%M:%S')
+            data = data_point(dt, humi, temp)
+            sleep(interval)
+    # If keyboard Interrupt is pressed
+    except KeyboardInterrupt:
+        pass  			# Go to next line
+
+
+if __name__ == "__main__":
+    print(get_dht22_data(5))
