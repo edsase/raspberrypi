22faf0f106b781a37c8f042643d220f0a5cfc67c
Edmond Sesay <edmondssesay@yahoo.com>
2019-02-11 17:38:26 +0100

    timestamp_motion_detection function added to motion_detection.py

--- motion_detection.py
+++ motion_detection.py
@@ -9,3 +9,22 @@ while True:
         print('Motion detected')
         blink(8)
 
+
+def timestamp_motion_detection():
+    from datetime import datetime
+    from collections import namedtuple
+
+    data = namedtuple('data', 'timestamp value')
+
+    # current datetime
+    dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    data = data(dt, 1)
+    print(data)
+
+    return data(dt, 1)
+    
+    
+if __name__ == "__main__":
+    # add rising edge detection on channel 23
+    GPIO.add_event_detect(23, GPIO.RISING, callback=timestamp_motion_detection)  
